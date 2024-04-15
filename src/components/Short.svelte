e5ea1afaae865df83fbd466ba3cdd13fbf54bcd8
Walter Diez <wdiez3@gmail.com>
2024-04-15 20:56:27 -0300

    Input con Tailwind

--- src/components/Short.svelte
+++ src/components/Short.svelte
@@ -1,6 +1,6 @@
 <script>
-    let hello = "hola";
+    let url = "";
+    const styleInput = "bg-gray-200 appeareance-none border-2 border-gray-200 rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:border-purple-500";
 </script>
 
-<h1>{hello}</h1>
-<input type="text" bind:value={hello}>
+<input type="text" placeholder="Example://www.mysite.com" bind:value={url} class={styleInput}/>
