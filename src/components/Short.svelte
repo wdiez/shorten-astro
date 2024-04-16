fc3075f0a0382ca650aa9b1bd8e4c85bfff67112
Walter Diez <wdiez3@gmail.com>
2024-04-15 21:46:51 -0300

    Recortar URL

--- src/components/Short.svelte
+++ src/components/Short.svelte
@@ -1,5 +1,28 @@
 <script>
+    //EIFF.SHORT.GY
+    //sk_EAAMAkIZA5pyNsO1
     let url = "";
+    let urlResult = "";
+    let show = false;
+
+    const fetchUrl = async () => {
+
+        const options = {
+            method: 'POST',
+            headers : {
+                accept: 'application/json',
+                'content-type': 'application/json',
+                Authorization: 'sk_EAAMAkIZA5pyNsO1'
+            },
+            body: JSON.stringify({originalURL: url, domain: 'eiff.short.gy'})
+        };
+
+        const response = await fetch('https://api.short.io/links', options);
+        const data = await response.json();
+        console.log(data);
+        urlResult = data.shortURL;
+    }
+
     const styleInput = "bg-gray-200 appeareance-none border-2 border-gray-200 rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:border-purple-500";
     const buttonStyle ="bg-purple-500 hover:bg-purple-400 text-white font-bold py-2 px-10 rounded"
 </script>
@@ -14,5 +37,7 @@
 </div>
 
 <div class="md:flex md:items-center mt-10 justify-center">
-    <button type="button" class={buttonStyle}>Shorten</button>
+    <button type="button" class={buttonStyle} on:click={fetchUrl}>Shorten</button>
 </div>
+
+<h1>{urlResult}</h1>
