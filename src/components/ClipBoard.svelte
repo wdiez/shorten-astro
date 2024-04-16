db9f1b3756cce8cd7716fc022bc2134dda949987
Walter Diez <wdiez3@gmail.com>
2024-04-15 22:20:29 -0300

    Boton copia URL

---
+++ src/components/ClipBoard.svelte
@@ -0,0 +1,13 @@
+<script>
+    import {onMount} from "svelte";
+    export let urlResult;
+
+    let textarea;
+
+    onMount(() => {
+        textarea.select();
+        document.execCommand("copy");
+    })
+</script>
+
+<textarea bind:value={urlResult} bind:this={textarea}></textarea>
--- src/components/Short.svelte
+++ src/components/Short.svelte
@@ -1,10 +1,17 @@
 <script>
-    //EIFF.SHORT.GY
-    //sk_EAAMAkIZA5pyNsO1
+    import ClipBoard from "./ClipBoard.svelte";
     let url = "";
     let urlResult = "";
     let show = false;
 
+    const copy = () => {
+        const app = new Clipboard({
+            target: document.getElementById("clipboard"),
+            props: {urlResult},
+        })
+        app.$destroy();
+    }
+    
     const fetchUrl = async () => {
 
         const options = {
@@ -21,6 +28,7 @@
         const data = await response.json();
         console.log(data);
         urlResult = data.shortURL;
+        show = true;
     }
 
     const styleInput = "bg-gray-200 appeareance-none border-2 border-gray-200 rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:border-purple-500";
@@ -29,7 +37,7 @@
 
 <div class="md:flex md:items-center md-6">
     <div class="md:w-1/3">
-        <label for="" class="block text-gray-500 font-bolt md:text-right pr-4">URL to shorten</label>
+        <label for="" class="block text-gray-500 font-bold md:text-right pr-4">URL to shorten</label>
     </div>
     <div class="md:w-1/3">
         <input type="text" placeholder="Example://www.mysite.com" bind:value={url} class={styleInput}/>
@@ -40,4 +48,10 @@
     <button type="button" class={buttonStyle} on:click={fetchUrl}>Shorten</button>
 </div>
 
-<h1>{urlResult}</h1>
+{#if show}
+    <div class="md:flex md:items-center mt-10 justify-center">
+        <input bind:value={urlResult} class="text-5xl text-pink-500">
+        <button class={buttonStyle} on:click={copy}>Copy</button>
+        <div id="clipboard"></div>
+    </div>
+{/if}
