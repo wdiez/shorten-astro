e5b5453b59b5347ab136b011e2dd3039671b2d27
Walter Diez <wdiez3@gmail.com>
2024-04-15 21:09:31 -0300

    Terminando diseño de input

--- src/components/Short.svelte
+++ src/components/Short.svelte
@@ -1,6 +1,18 @@
 <script>
     let url = "";
     const styleInput = "bg-gray-200 appeareance-none border-2 border-gray-200 rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:border-purple-500";
+    const buttonStyle ="bg-purple-500 hover:bg-purple-400 text-white font-bold py-2 px-10 rounded"
 </script>
 
-<input type="text" placeholder="Example://www.mysite.com" bind:value={url} class={styleInput}/>
+<div class="md:flex md:items-center md-6">
+    <div class="md:w-1/3">
+        <label for="" class="block text-gray-500 font-bolt md:text-right pr-4">URL to shorten</label>
+    </div>
+    <div class="md:w-1/3">
+        <input type="text" placeholder="Example://www.mysite.com" bind:value={url} class={styleInput}/>
+    </div>
+</div>
+
+<div class="md:flex md:items-center mt-10 justify-center">
+    <button type="button" class={buttonStyle}>Shorten</button>
+</div>
